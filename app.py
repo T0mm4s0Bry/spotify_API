905354d1b5094f1995754eead104238869fbada0
T0mm4s0Bry <149154826+T0mm4s0Bry@users.noreply.github.com>
2025-01-30 08:48:32 +0000

    fatto spotify

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, redirect, request, url_for, render_template,session
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+#le tue credenziali le trovi nella dashboard di prima
+SPOTIFY_CLIENT_ID = "4a2cbb97c5504b7dbe7a2efc0c840bf2"
+SPOTIFY_CLIENT_SECRET = "0fb3aa1797704da5925b662d34732f95"
+SPOTIFY_REDIRECT_URI = "http://127.0.0.1:5000/callback" #dopo il login andiamo qui
+
+app = Flask(__name__)
+app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
+
+#config SpotifyOAuth per l'autenticazione e redirect uri
+sp_oauth = SpotifyOAuth(
+client_id=SPOTIFY_CLIENT_ID,
+client_secret=SPOTIFY_CLIENT_SECRET,
+redirect_uri=SPOTIFY_REDIRECT_URI,
+scope="user-read-private" #permessi x informazioni dell'utente
+)
+
+@app.route('/')
+def login():
+    auth_url = sp_oauth.get_authorize_url() #login di spotify
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code') #recupero codice di autorizzazione
+    token_info = sp_oauth.get_access_token(code) #uso il code per un codice di accesso
+    session['token_info'] = token_info #salvo il token nella mia sessione x riutilizzarlo
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    token_info = session.get('token_info', None) #recupero token sissione (salvato prima)
+    if not token_info:
+        return redirect(url_for('login'))
+    sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
+    user_info = sp.current_user()
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
+    print(user_info) #capiamo la struttura di user_info per usarle nel frontend
+    return render_template('home.html', user_info=user_info, playlists=playlists_info)
+
+#config SpotifyOAuth per l'autenticazione e redirect uri
+sp_oauth = SpotifyOAuth(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
+)
+
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
